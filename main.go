defcb2955193b21515d4908fa3d0793598cc830a
师文斌 <1011214027@qq.com>
2023-10-14 15:32:09 +0800

    test server

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"paj/my_web"
+)
 
 func main() {
 	println("hello paj!")
 
-	r := gin.Default()
-	r.GET("/test", func(c *gin.Context) {
-		c.String(200, "Hello, Geektutu")
+	//r := gin.Default()
+	//r.GET("/test", func(c *gin.Context) {
+	//	c.String(200, "Hello, Geektutu")
+	//})
+	////r.Run()
+	//r.Run("0.0.0.0:8004") // listen and serve on 0.0.0.0:8080
+
+	s := my_web.NewHTTPServer()
+	s.Get("/test", func(ctx *my_web.Context) {
+		ctx.Resp.Write([]byte("hello, my world"))
 	})
-	//r.Run()
-	r.Run("0.0.0.0:8004") // listen and serve on 0.0.0.0:8080
+
+	s.Start(":8004")
 }
